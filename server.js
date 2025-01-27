00eb225b46505098fc71a7b37155ab2f11deb760
benarivo66 <iz4comed@yahoo.com>
2025-01-27 19:15:23 +0100

    sanitization and validation

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ const static = require("./routes/static");
 const expressLayouts = require("express-ejs-layouts");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const accRoute = require("./routes/accountRoute");
 const intErrRoute = require("./routes/intErrRoute");
 const utilities = require("./utilities");
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
+
 
 /* ***********************
  * View Engine and Templates
@@ -22,12 +27,34 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // not at views root
 
+
+
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 /* ***********************
  * Routes
  *************************/
 app.use(static);
 app.get("/", utilities.handleErrors(baseController.buildHome));
 app.use("/inv", inventoryRoute);
+app.use("/account", accRoute)
 app.use("/intentionalError", intErrRoute)
 app.use(async (req, res, next) => {
   next({ status: 404, message: "Sorry, we appear to have lost that page." });
